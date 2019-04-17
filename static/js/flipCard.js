31b01b097247e594b54be7a19c2b22089140b57a
Kolos Gergely <kolos460@gmail.com>
2019-04-17 11:43:51 +0200

    merged game functions + music

--- static/js/flipCard.js
+++ static/js/flipCard.js
@@ -1,9 +1,35 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 for (card of cards) {
     card.addEventListener('click', flipCard);
 }
 
 function flipCard() {
     this.classList.toggle('flip');
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 16);
+    card.style.order = randomPos;
+  });
+})();
+
+function checkMatch() {
+    let isMatch
+    if isMatch {
+
+    }
+
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 }
