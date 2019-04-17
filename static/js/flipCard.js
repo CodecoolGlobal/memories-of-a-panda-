7813e016b8272131e1110ccc649e7f10e38dd5fb
Milán <matolcsi.milan@gmail.com>
2019-04-17 17:56:15 +0200

    3 levels started

--- static/js/flipCard.js
+++ static/js/flipCard.js
@@ -1,12 +1,60 @@
-const cards = document.querySelectorAll(".memory-card");
+var level1, level2;
 
-for (card of cards) {
-    card.addEventListener('click', flipCard);
+
+function create_duplicate_html_card (card_name) {
+    function create_html_card (card_name) {
+    let front = document.createElement("img");
+    let back = document.createElement("img");
+    let card = document.createElement("div");
+    card.className = "memory-card";
+    card.setAttribute("data-cardname", `${card_name}`);
+    front.className = "front-face";
+    front.src = "static/img/" + card_name + ".gif";
+    back.className = "back-face";
+    back.src = "static/img/adventurecardback.png";
+    card.appendChild(front);
+    card.appendChild(back);
+    return card;
+    }
+    let game = document.getElementsByClassName('memory-game');
+    game[0].appendChild(create_html_card(card_name));
+    game[0].appendChild(create_html_card(card_name));
 }
+
+
+function pick_card () {
+    let audio = new Audio("/static/music/explorer.mp3");
+    audio.pause();
+    audio.play();
+    level1 = true;
+    if (level1) {
+        var arrayOfCards = ["stalagg", "emperor", "feugen"]
+    }
+    else if (level2) {
+        arrayOfCards = ["stalagg", "emperor", "feugen", "kelthuzad", "loatheb", "majordomo"]; ///"shade", "baron"];
+    }
+    else {
+        arrayOfCards = []
+    }
+    for (let i = 0; i < arrayOfCards.length; i++) {
+        create_duplicate_html_card(arrayOfCards[i])
+    }
+    addEventListeners();
+}
+
+
+function checkWin () {
+    let flippedCards = document.querySelectorAll(".memory-card");
+    for (card of flippedCards) {
+        console.log(card)
+    }
+}
+
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+
 
 function flipCard() {
     if (lockBoard) return;
@@ -25,6 +73,7 @@ function flipCard() {
     secondCard = this;
 
     checkForMatch();
+    checkWin();
 }
 
 function checkForMatch() {
@@ -52,16 +101,26 @@ function checkForMatch() {
 }
 
 function disableCards() {
-        firstCard.removeEventListener('click', flipCard)
+        firstCard.removeEventListener('click', flipCard);
         secondCard.removeEventListener('click', flipCard)
     }
 
 function unflipCards() {
     lockBoard = true;
     setTimeout(() => {
-        firstCard.classList.remove('flip')
-        secondCard.classList.remove('flip')
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
         firstCard = "";
         lockBoard = false;
     }, 1500)
 }
+
+
+function addEventListeners () {
+    let cards = document.querySelectorAll(".memory-card");
+    for (card of cards) {
+        card.addEventListener('click', flipCard);
+    }
+}
+
+pick_card();
